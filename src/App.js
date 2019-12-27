2548f890ed88f150768d8de857fad54fe77e8c9f
Nick Galluzzo <n.galluzzo@gmail.com>
2019-12-27 18:54:16 -0500

    add items to cart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Nav from './Nav';
 import './App.css';
+import {items} from './static-data';
+import ItemPage from './ItemPage';
+import CartPage from './CartPage'
 
 class App extends React.Component {
   state = {
-    activeTab: 0
+    activeTab: 0,
+    cart: []
   };
 
   handleTabChange = (index) => {
@@ -13,11 +17,44 @@ class App extends React.Component {
     });
   }
 
+  handleAddToCart = (item) => {
+    this.setState({
+      cart: [...this.state.cart, item.id]
+    })
+  }
+
+  renderCart() {
+    let itemCounts =
+      this.state.cart.reduce((itemCounts, itemId) => {
+      itemCounts[itemId] = itemCounts[itemId] || 0;
+      itemCounts[itemId]++;
+      return itemCounts;
+    }, {});
+
+    let cartItems =
+      Object.keys(itemCounts).map(itemId => {
+        let item = items.find(item =>
+          item.id === parseInt(itemId, 10)
+        );
+        return {
+          ...item,
+          count: itemCounts[itemId]
+      }
+      });
+
+      return (
+        <CartPage items={cartItems} />
+      );
+  }
+
   renderContent() {
     switch(this.state.activeTab) {
       default:
-      case 0: return <span>Items</span>
-      case 1: return <span>Cart</span>
+      case 0: return <ItemPage
+                       items={items}
+                       onAddToCart={this.handleAddToCart}
+                       />
+      case 1: return this.renderCart();
     }
   }
 
